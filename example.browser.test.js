0ef7d14758ec836cb437756e9468d884efc74681
Nicholas Vuono <nicholas.vuono@gmail.com>
2023-05-13 05:12:03 -0400

    a few cahnges to try to get it to run

--- example.browser.test.js
+++ example.browser.test.js
@@ -13,6 +13,7 @@ export default async function () {
   describe('Example Browser test @performance @browser @example @login', async () => {
     const browser = chromium.launch({
       headless: true,
+      timeout: 120000,
     });
     const page = browser.newPage();
 
